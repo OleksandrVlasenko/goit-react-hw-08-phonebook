8d5750728f3e94508d412847089f8183fcc85ec8
OleksandrVlasenko <sania.rapsik@gmail.com>
2023-07-01 23:41:33 +0300

    fixbug in styles

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -14,7 +14,7 @@ export const UserMenu = () => {
       <Button
         variant="outlined"
         onClick={() => dispatch(logOut())}
-        sx={{ color: '#05888d', border: '1px solid #05888d7f' }}
+        sx={{ color: '#05888d', border: '1px solid #05888d7f', textTransform: "none" }}
       >
         Logout
       </Button>
